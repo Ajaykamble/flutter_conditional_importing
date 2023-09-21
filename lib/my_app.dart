918b00000ca2270bf4d320bda065e070ecd8a35f
Ajay Kamble <ajay.kamble119@gmail.com>
2023-09-21 12:55:58 +0530

    added flutter code

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -13,6 +13,36 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   
+  displayAlert(String message) async {
+    if (!kIsWeb) {
+      switch (defaultTargetPlatform) {
+        case TargetPlatform.iOS:
+          showDialog(
+            context: context,
+            builder: (_) => CupertinoAlertDialog(
+              content: Text(message),
+            ),
+          );
+          break;
+        case TargetPlatform.windows:
+          const methodChannel = MethodChannel('dev.fluttercrew');
+          await methodChannel.invokeMethod("displayAlert", {"message": message});
+          break;
+        case TargetPlatform.android:
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              content: Text(message),
+            ),
+          );
+          break;
+        default:
+          break;
+      }
+    } else {
+      js.context.callMethod("displayAlert", [message]);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
